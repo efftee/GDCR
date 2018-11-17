329ec18b275a9d03c621729b839a4a2d6ead1ef2
Fernando Cerullo <fernando.cerullo@digi.com>
2018-11-17 14:23:10 -0500

    Calculate next state for true current state

--- conway.py
+++ conway.py
@@ -3,6 +3,10 @@ from unittest import TestCase
 import numpy as np
 from scipy.signal import convolve as sconv
 
+
+GOL_KERNEL = np.array([[1, 1, 1],
+                           [1, 0, 1],
+                           [1, 1, 1]])
 class ConwayGOL(TestCase):
 
     # def test_simple(self):
@@ -34,6 +38,18 @@ class ConwayGOL(TestCase):
         output = convolve(matrix, kernel)
         self.assertTrue(np.array_equal(output, result))
 
+    def test_alive_neighbors(self):
+        cases = range(9)
+        for case in cases:
+            next_state = get_next_state(1, case)
+            if case<2 or case > 3:
+                self.assertEqual(0, next_state)
+            else:
+                self.assertEqual(1, next_state)
+        
+
+
+
 def update(mat) -> np.ndarray:
     """
     Convovle over mat with kernel X
@@ -41,5 +57,14 @@ def update(mat) -> np.ndarray:
     """
     return mat
 
-def convolve(mat, kernel):
+
+def get_next_state(current_state, alive_neighbors):
+    if current_state==1:
+        if alive_neighbors < 2 or alive_neighbors > 3:
+            return 0
+        else:
+            return 1
+
+
+def convolve(mat, kernel=GOL_KERNEL):
     return sconv(mat, kernel, mode='same')
