bc0680d1693d2c14e131c8252cc6262568038b49
tnguyen <tnguyen@cargurus.com>
2018-11-17 14:06:33 -0500

    convolve test passed

--- conway.py
+++ conway.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
 import numpy as np
+from scipy.signal import convolve as sconv
 
 class ConwayGOL(TestCase):
 
@@ -20,6 +21,25 @@ class ConwayGOL(TestCase):
         output = update(matrix)
         self.assertEqual(output.shape, matrix.shape)
 
+    def test_convolve(self):
+        matrix = np.array([[1, 1, 0],
+                           [0, 0, 0],
+                           [0, 1, 0]])
+        kernel = np.array([[1, 1, 1],
+                           [1, 0, 1],
+                           [1, 1, 1]])
+        result = np.array([[1, 1, 1],
+                           [3, 3, 2],
+                           [1, 0, 1]])
+        output = convolve(matrix, kernel)
+        self.assertTrue(np.array_equal(output, result))
 
 def update(mat) -> np.ndarray:
+    """
+    Convovle over mat with kernel X
+    Compare  with cutoffs
+    """
     return mat
+
+def convolve(mat, kernel):
+    return sconv(mat, kernel, mode='same')
